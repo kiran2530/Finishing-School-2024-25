8f1ac309bbed45f09c6106b9cb67db74ce3cafdb
kirannikam <nikamsarkar464@gmail.com>
2025-01-09 22:11:01 +0530

    learned lazy loading

--- angular/myapp/src/app/app-routing.module.ts
+++ angular/myapp/src/app/app-routing.module.ts
@@ -1,18 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    component: HomeComponent,
-    children: [
-      { path: 'about', component: AboutComponent }, 
-    ],
-  },
-  { path: '', redirectTo: '/home', pathMatch: 'full' }, 
-  { path: '**', redirectTo: '/home' }, 
+  { path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
+  { path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule) },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', redirectTo: 'home' } // Wildcard route for undefined paths
 ];
 
 @NgModule({
--- angular/myapp/src/app/app.module.ts
+++ angular/myapp/src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent
   ],
   imports: [
     BrowserModule,
